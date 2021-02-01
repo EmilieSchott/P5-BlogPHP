82c2ca3fa0320c83bc82bbf90d8900f499a1f5a2
Emilie Schott <emilie.schott@gmail.com>
2021-02-01 18:13:12 +0100

    menu modification in base.twig + router cleaning

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
 require_once 'vendor/autoload.php';
 
-//Routing
-
-
-
 // template twig loading :
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/view');
 $twig = new \Twig\Environment($loader, [
     'cache' => false // __DIR__ . '/tmp',
 ]);
 
-// en attendant de le mettre dans le contrôleur
+//Routing
 echo $twig->render('homeView.twig');
+
