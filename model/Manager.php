5995df72a8f6fa5aaee25dfa173d19755d0ae0ae
Emilie Schott <emilie.schott@gmail.com>
2021-02-26 19:08:04 +0100

    Add 2 methods in Manager.php : paginator() to define the pages of a request results + displayPage() to a display a specific page of a request results.

--- model/Manager.php
+++ model/Manager.php
@@ -1,15 +1,33 @@
 <?php
 
-// Define namespace of this class :
 namespace EmilieSchott\BlogPHP\Model;
 
-// Define this class :
 abstract class Manager {
-    
     protected $db;
     
     public function __construct() {
-        $db=new \PDO('mysql:host=localhost;dbname=p5_blog_php;port=3308;charset=utf8', 'root', '', array(\PDO::ATTR_ERRMODE=>\PDO::ERRMODE_EXCEPTION));
+        $db=new \PDO('mysql:host=localhost;dbname=p5_blog_php;port=3308;charset=utf8', 'root', '');
         $this->db=$db;
+    }
+
+    public function paginator(array $datas, int $itemsPerPage): array {
+        $datasPages = \array_chunk($datas, $itemsPerPage);
+        \array_unshift($datasPages, '');
+        unset($datasPages[0]);
+        $pagesNbr = \array_key_last($datasPages);
+
+        $paginator = [
+        'datasPages' => $datasPages,
+        'pagesNbr' => $pagesNbr
+        ];
+
+        return $paginator;
+    }
+
+    public function displayPage(array $datasPages, int $page): array {
+    $offset=$page-1;
+    $datasPage=array_slice($datasPages, $offset, 1);
+    $datasPage=$datasPage[0];
+    return $datasPage;
     }
 }
